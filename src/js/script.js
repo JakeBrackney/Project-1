15da9fa0f1b3c7248ebc3e7fd9c481e1c541c383
Jake Brackney <brackney.jake@gmail.com>
2019-01-08 12:32:13 -0500

    Pseudocode and some styling changes

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+// rings, array?
+const rings = [1, 2, 3]
+
+let towerOne = []
+let towerTwo = []
+let towerThree = []
+
+// Game moves = push piece from one array to the next
+
+function changeTowers() {
+
+}
+
+//logic to compare size of the ring being moved vs the ring it's being placed on
+
+if (ring1 < ring2) {
+
+}
+
+
+// logic to evaluate win status (all rings on towerThree)
+
+function gameOver() {
+    if (towerThree = rings) {
+    console.log('Winner!')
+    }
+}
+
+
+
+// Bonus: move counter:
+
+// Bonus: stopwatch
+
+// Bonus: Add rings
+
+
+
+
